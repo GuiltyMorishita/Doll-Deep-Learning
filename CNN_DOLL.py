416d95292990b4e1c0191792958e453b94a82882
GuiltyMorishita <morishita@lang.cs.tut.ac.jp>
2016-06-17 03:21:47 +0900

    採点プログラム追加

--- CNN_DOLL.py
+++ CNN_DOLL.py
@@ -16,9 +16,9 @@ class ImageNet(Chain):
             conv1 =  L.Convolution2D(3, 32, 5), #padding=0, stride=1 filtersize=5
             conv2 =  L.Convolution2D(32, 32, 5),
 
-            l3 =     L.Linear(512, 512), #32
+            # l3 =     L.Linear(512, 512), #32
             # l3 =     L.Linear(1568, 512), #50
-            # l3 =     L.Linear(2592, 512), #64
+            l3 =     L.Linear(2592, 512), #64
             l4 =     L.Linear(512, n_outputs)
         )
 
@@ -48,14 +48,13 @@ class ImageNet(Chain):
                 if y_data[cnt] != np.argmax(ydata):
                     # 識別に失敗したデータを出力する処理．
                     missid.append(glob_z_test[z_batch[cnt]])
-                    cnt += 1
 
-                glob_all_missid.extend(missid)
+                cnt += 1
+
+            glob_all_missid.extend(missid)
                 # 全バッチにおいて識別失敗した id を格納
 
         return F.softmax_cross_entropy(y, t), F.accuracy(y,t)
-
-
 
 class CNN:
     def __init__(self, data, target, n_outputs, gpu=-1):
@@ -74,7 +73,7 @@ class CNN:
         self.x_train,\
         self.x_test,\
         self.y_train,\
-        self.y_test, \
+        self.y_test,\
         self.z_train,\
         glob_z_test = train_test_split(data, target, file_ids, test_size=0.1)
 
@@ -93,6 +92,9 @@ class CNN:
         epoch = 1
         best_accuracy = 0
         log = ""
+
+
+
         while epoch <= n_epoch:
             print 'epoch', epoch
             log = log + 'epoch ' + str(epoch) + '\n'
@@ -100,10 +102,13 @@ class CNN:
             perm = np.random.permutation(self.n_train)
             sum_train_accuracy = 0
             sum_train_loss = 0
+
+            global glob_all_missid
+            glob_all_missid = []
+
             for i in xrange(0, self.n_train, batchsize):
                 x_batch = self.x_train[perm[i:i+batchsize]]
                 y_batch = self.y_train[perm[i:i+batchsize]]
-
                 real_batchsize = len(x_batch)
 
                 self.optimizer.zero_grads()
@@ -124,6 +129,9 @@ class CNN:
             for i in xrange(0, self.n_test, batchsize):
                 x_batch = self.x_test[i:i+batchsize]
                 y_batch = self.y_test[i:i+batchsize]
+
+                global z_batch
+                z_batch = range(i, i+batchsize)
 
                 real_batchsize = len(x_batch)
 
